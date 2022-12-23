83da4fedbdb1dbaad2d0f1dd13e8e725c2aceda3
Prabhakar Kumar <prabhakk@mathworks.com>
2022-12-23 16:28:58 +0530

    update tests for new icon

--- src/jupyter_matlab_proxy/__init__.py
+++ src/jupyter_matlab_proxy/__init__.py
@@ -38,9 +38,6 @@ def setup_matlab():
     logger = mwi_logger.get(init=True)
     logger.info("Initializing Jupyter MATLAB Proxy")
 
-    # Get MATLAB icon from matlab_proxy
-    #package_path = Path(inspect.getfile(matlab_proxy)).parent
-    #icon_path = package_path / "icons" / "matlab.svg"
     icon_path = Path(__file__).parent / "icon_open_matlab.svg"
     logger.debug(f"Icon_path:  {icon_path}")
     logger.debug(f"Launch Command: {matlab_proxy.get_executable_name()}")
